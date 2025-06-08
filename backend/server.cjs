573d256a6dc637b16413814973ec35992fc31387
leafboo <janjancaranguian@gmail.com>
2025-06-08 16:08:24 +0800

    Remove the port number in the backend code

--- backend/server.cjs
+++ backend/server.cjs
@@ -1,9 +1,10 @@
 console.log("Server is running...")
-const io = require('socket.io')(3000, {
+const io = require('socket.io')/*(3000, {
   cors: {
     origin: true // this allows any URL to make a request
   }
 }) // Apparently this is a function????
+ */ //idk if this is needed, the "3000" when deploying the code
 
 io.on('connection', socket => { // runs everytime a client connects to a server and gives them a 'socket' instance
   let clientUsername // every client has this variable instance
